be6f7d75e324a28f9c4ad8fbaec9a9ae0e74a4ec
Ionut Colceriu <contact@ghinda.net>
2016-10-21 12:53:28 +0300

    Remove comment about updating only text content on text nodes. Changing nodeValue has slightly worse than using replaceNode, on text nodes. Performance is probably bad also because we have to manually remove event listeners this way.

--- src/dom.js
+++ src/dom.js
@@ -59,7 +59,6 @@ export function diff ($node, $newNode, patches = []) {
   // if one of them is not an element node,
   // or the tag changed,
   // or not the same number of children.
-  // TODO compare text nodes.
   if ($node.nodeType !== 1 ||
     $newNode.nodeType !== 1 ||
     $node.tagName !== $newNode.tagName ||
